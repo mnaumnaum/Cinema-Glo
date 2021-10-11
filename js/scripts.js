2631934166999b51a0acc6894c56779097f27465
mnaumnaum <61330417+mnaumnaum@users.noreply.github.com>
2021-10-11 23:55:00 +0300

    Написала CSS для адаптивности

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag =
   document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 400;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -11,4 +13,8 @@ schemeSvg.addEventListener("click", (event) => {
     totalPrice = totalSeats * cost;
     totalPriceTag.textContent = totalPrice;
   }
+});
+menuButton.addEventListener("click", () => {
+  console.log("Кликнули по меню");
+  menu.classList.toggle("is-open");
 });
